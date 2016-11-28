ec2a721006e95ca66293cf5d9ad56f4f10c41679
Tim Cigrand <tcigrand@apto.com>
2016-11-28 11:04:03 -0700

    Polygon not included in this version and add all options for ng2-map.component

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -5,8 +5,7 @@ import { Ng2Map } from './services/ng2-map';
 import { Ng2MapComponent } from './components/ng2-map.component';
 import { Marker } from './directives/marker';
 import { Circle } from './directives/circle';
-import { Polygon } from './directives/polygon';
 import { InfoWindow } from './components/info-window';
-export { OptionBuilder, GeoCoder, NavigatorGeolocation, Ng2Map, Ng2MapComponent, Marker, Circle, Polygon, InfoWindow };
+export { OptionBuilder, GeoCoder, NavigatorGeolocation, Ng2Map, Ng2MapComponent, Marker, Circle, InfoWindow };
 export declare class Ng2MapModule {
 }
